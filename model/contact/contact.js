0d4b40c57caf1d8c16f9a6638caac5b6a2f90dce
Mike-Max <scartman1992@gmail.com>
2021-11-05 23:05:41 +0000

    update schema

--- model/contact/contact.js
+++ model/contact/contact.js
@@ -1,8 +1,29 @@
 const { Schema, model } = require('mongoose')
+const codeRegexp = /^[0-9]{10}$/
 const contactsSchema = Schema({
-  name: String,
-  phone: String,
-  email: String,
+  name: {
+    type: String,
+    minLength: 2,
+    maxLength: 30,
+    required: true,
+  },
+  phone: {
+    type: String,
+    minLength: 2,
+    maxLength: 30,
+    required: true,
+  },
+  email: { type: String, minLength: 2, maxLength: 30, required: true },
+  code: {
+    type: String,
+    required: true,
+    match: codeRegexp,
+  },
+  //   status: {
+  //     type: String,
+  //     enum: ['stock', 'for order', 'discounts'],
+  //     default: 'stock',
+  //   },
 })
 
 const contact = model('contacts', contactsSchema)
