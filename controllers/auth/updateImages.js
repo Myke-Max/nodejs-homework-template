177992ac0dd821523f3459272f3a589df832bbf8
Mike-Max <scartman1992@gmail.com>
2021-11-18 23:38:31 +0000

    update jimp

--- controllers/auth/updateImages.js
+++ controllers/auth/updateImages.js
@@ -2,6 +2,7 @@ const { User } = require('../../model/auth')
 const fs = require('fs/promises')
 const { successResponse } = require('../../helpers')
 const path = require('path')
+const jimp = require('jimp')
 
 const usersImageDir = path.join(__dirname, '../../', 'public/users')
 const updateImages = async (req, res, next) => {
@@ -12,9 +13,11 @@ const updateImages = async (req, res, next) => {
     const resultUpload = path.join(usersImageDir, `${id} ${originalname}`)
     await fs.rename(tempDir, resultUpload)
     const image = path.join('/avatars', `${id} ${originalname}`)
-    const result = await User.findByIdAndUpdate(id, { image })
-
-    successResponse(res, result)
+    const resizeAvatar = await jimp.read(resultUpload)
+    resizeAvatar.resize(256, 256).write(resultUpload)
+    await User.findByIdAndUpdate(id, { image })
+    req.user.avatarURL = image
+    successResponse(res, image)
   } catch (error) {
     fs.unlink(tempDir)
     next(error)
